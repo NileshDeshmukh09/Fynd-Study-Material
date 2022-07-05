547a679c75917a8b6065e13f4a25c3d2c8640c51
Prashanth Puranik <puranik@digdeeper.in>
2022-07-05 20:14:50 +0530

    pagination

--- projects-in-progress/workshops-app-js/public/js/pages/workshops-list.js
+++ projects-in-progress/workshops-app-js/public/js/pages/workshops-list.js
@@ -1,9 +1,13 @@
+let page = 1;
+
 const fetchWorkshops = async () => {
-    const response = await fetch( `https://workshops-server.herokuapp.com/workshops` );
+    const response = await fetch( `https://workshops-server.herokuapp.com/workshops?` + new URLSearchParams({
+        _page: page
+    }));
     
     // take care of cases when backend returns an error - we need to throw the error from this function ourselves
     if( !response.ok ) {
-        const responseText = await response.text();
+        const responseText = await response.text(); // get the text error message from the backend
         throw new Error( responseText || 'Some error occured' );
     }
 
@@ -50,9 +54,9 @@ const showWorkshops = ( workshops ) => {
     workshopsListEl.innerHTML = workshopsListStr;
 };
 
-window.addEventListener( 'load', async function() {
+const fetchAndShowWorkshops = async () => {
     try {
-        const workshops = await fetchWorkshops();    
+        const workshops = await fetchWorkshops();
         showWorkshops( workshops );
     } catch( error ) {
         const errorMessage = document.getElementById( 'error-message' );
@@ -63,4 +67,26 @@ window.addEventListener( 'load', async function() {
     // hide the loading message now...
     const loadingMessage = document.getElementById( 'loading-message' );
     loadingMessage.classList.add( 'd-none' );
+};
+
+const setupPagination = () => {
+    const nextPageBtn = document.getElementById( 'next-page' );
+    const previousPageBtn = document.getElementById( 'previous-page' );
+
+    nextPageBtn.addEventListener( 'click', function() {
+        page = page + 1;
+        fetchAndShowWorkshops();
+    });
+    
+    previousPageBtn.addEventListener( 'click', function() {
+        page = page - 1;
+        fetchAndShowWorkshops();
+    });
+};
+
+// window.addEventListener( 'load', async function() {
+// DOMContentLoaded fires when the DOM is ready (happens before load event)
+document.addEventListener( 'DOMContentLoaded', function() {
+    setupPagination();
+    fetchAndShowWorkshops(); 
 });
