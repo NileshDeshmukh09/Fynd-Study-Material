ee3c791af0e5315956d41026e6e6eb710c46d7f8
Prashanth Puranik <puranik@digdeeper.in>
2022-07-05 18:24:55 +0530

    workshops list displayed

--- projects-in-progress/workshops-app-js/public/js/pages/workshops-list.js
+++ projects-in-progress/workshops-app-js/public/js/pages/workshops-list.js
@@ -4,7 +4,46 @@ const fetchWorkshops = async () => {
     return workshops;
 };
 
+const showWorkshops = ( workshops ) => {
+    const workshopsListEl = document.querySelector( '.workshops-list' );
+
+    let workshopsListStr = '';
+
+    workshops.forEach(
+        workshop => {
+            const {
+                imageUrl,
+                name,
+                startDate,
+                endDate,
+                time,
+                description
+            } = workshop;
+
+            const workshopStr = `
+                <a class="workshops-list-item link-plain" href="/workshop-details.html">
+                    <img src="${imageUrl}" alt="${name}" class="img-responsive img-workshop" />
+                    <div class="workshop-title mt-3 mb-3">
+                        ${name}
+                    </div>
+                    <div class="workshop-date mt-2 mb-2">
+                        ${startDate} - ${endDate}
+                    </div>
+                    <div class="workshop-time mt-2 mb-2">
+                        ${time}
+                    </div>
+                </a>
+            `;
+
+            workshopsListStr += workshopStr;
+
+        }
+    );
+
+    workshopsListEl.innerHTML = workshopsListStr;
+};
+
 window.addEventListener( 'load', async function() {
     const workshops = await fetchWorkshops();
-    console.log( workshops );
+    showWorkshops( workshops );
 });
