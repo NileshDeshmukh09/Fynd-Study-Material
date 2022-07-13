b2acac5db70789a1bbfe8c61da79940c05e910ce
Prashanth Puranik <puranik@digdeeper.in>
2022-07-13 19:34:01 +0530

    started with Vue project

---
+++ projects-in-progress/workshops-app-vue/src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      Hello Vue CLI project
+    </div>
+</template>
+
+<script>
+const HelloWorld = {
+  name: 'HelloWorld',
+}
+
+export default HelloWorld;
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
