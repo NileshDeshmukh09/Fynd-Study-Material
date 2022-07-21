ef1a31a18dfebea2a179161cc768418950473eae
Prashanth Puranik <puranik@digdeeper.in>
2022-07-21 17:51:47 +0530

    simple web server

---
+++ demos/05-nodejs/01-basics/01-simple-web-server.js
@@ -0,0 +1,9 @@
+// import http from 'http';
+const http = require( 'http' );
+
+// asynchronous non-blocking function
+const server = http.createServer(( req, res ) => {
+    res.end( 'Hello Node' );
+});
+
+server.listen( 8080 );
