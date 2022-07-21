b7c750895c86605f498e4956111e969c7ffb8f52
Prashanth Puranik <puranik@digdeeper.in>
2022-07-21 19:19:16 +0530

    comments

--- demos/05-nodejs/02-modules/a.js
+++ demos/05-nodejs/02-modules/a.js
@@ -1,4 +1,7 @@
+// b.js will execute and in turn execute c.js
 const b = require( './b' );
+
+// c.js will NOT execute. The value of its exports will be returned.
 const c = require( './c' );
 
 console.log( 'a started executing' );
