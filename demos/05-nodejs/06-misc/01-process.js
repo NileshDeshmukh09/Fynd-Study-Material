b208b06b1332bc463f2f3d9d06dac81363ad31d2
Prashanth Puranik <puranik@digdeeper.in>
2022-07-22 20:51:56 +0530

    process

--- demos/05-nodejs/06-misc/01-process.js
+++ demos/05-nodejs/06-misc/01-process.js
@@ -11,7 +11,10 @@ console.log( process.pid );
 // if you use this mechanism then make sure your errors do not go unnoticed - for example you can log the errors to a separate file
 process.on( 'uncaughtException', error => {
     console.error( error.message ); // better save the error message to a file
-    // process.exit(0);
+    
+    // send a positive number to indicate exit due to errors.
+    // send 0 to indicate normal exit (program completed its job successfuly and is exiting)
+    process.exit( 1 );
 });
 
 const server = http.createServer((req, res) => {
