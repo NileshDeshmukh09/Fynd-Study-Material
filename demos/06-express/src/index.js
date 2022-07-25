816dd51a3f7b3ed3e0c8b05c10381f749d17438e
Prashanth Puranik <puranik@digdeeper.in>
2022-07-25 18:41:34 +0530

    routing

--- demos/06-express/src/index.js
+++ demos/06-express/src/index.js
@@ -3,11 +3,21 @@ const express = require( 'express' );
 // Application object (has a web server within)
 const app = express();
 
+// Method = GET
+// req, res -> same as in Node JS but with extra features
+app.get( '/', ( req, res ) => {
+    // res.send() adds Response header - 'Content-Type': 'text/html'
+    res.send( 'This is the workshops app' );
+});
+
+// EXERCISE: Handle /about and send something about the app.
+
 const PORT = process.env.PORT || 3000;
 
 app
     .listen( PORT, () => {
         console.log( `Server running on http://localhost:${PORT}` );
-    }).on( 'error', error => {
+    }) // listen() returns server
+    .on( 'error', error => { // server.on( ... )
         console.error( error.message );
     });
