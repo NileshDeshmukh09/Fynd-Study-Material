d15b8d2c108b28eeaf9684367e4e008daa2135c2
Prashanth Puranik <puranik@digdeeper.in>
2022-07-25 17:27:50 +0530

    exercise on command line args

--- demos/05-nodejs/06-misc/03-command-line-arguments.js
+++ demos/05-nodejs/06-misc/03-command-line-arguments.js
@@ -1,4 +1,21 @@
 // first 2 arguments are node and the script path
-console.log( process.argv );
-console.log( process.argv[2] );
-console.log( process.argv[3] );
+console.log(process.argv);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+
+const a = parseInt(process.argv[3]);
+const b = parseInt(process.argv[4]);
+switch (process.argv[2]) {
+    case "add":
+        console.log(a + b);
+        break;
+    case "substract":
+        console.log(a - b);
+        break;
+    case "multiply":
+        console.log(a * b);
+        break;
+    case "divide":
+        console.log(a / b);
+        break;
+}
