b8a8ae866a052611b11172c1e808e33f8809e6ad
Prashanth Puranik <puranik@digdeeper.in>
2022-07-25 17:16:37 +0530

    command line arguments

---
+++ demos/05-nodejs/06-misc/02-reading-environment-variables.js
@@ -0,0 +1,2 @@
+// an object with the a copy of all the environment variables
+console.log( process.env.PORT_NUMBER );
---
+++ demos/05-nodejs/06-misc/03-command-line-arguments.js
@@ -0,0 +1,4 @@
+// first 2 arguments are node and the script path
+console.log( process.argv );
+console.log( process.argv[2] );
+console.log( process.argv[3] );
