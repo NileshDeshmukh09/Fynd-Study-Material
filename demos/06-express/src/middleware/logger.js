da62148413a17acf263bebbf4c569151cfd6c6f5
Prashanth Puranik <puranik@digdeeper.in>
2022-07-26 20:20:17 +0530

    response from middleware

--- demos/06-express/src/middleware/logger.js
+++ demos/06-express/src/middleware/logger.js
@@ -2,14 +2,20 @@ const logger = ( req, res, next ) => {
     const date = (new Date()).toString();
     const url = req.url;
     const method = req.method;
+    const ip = req.ip;
+    const userAgent = req.headers['user-agent'];
 
     // EXERCISE: How to get the IP address??
     // EXERCISE: How to get the user agent from the request header??
 
-    const message = `${method} ${url} | Time: ${date}`;
+    const message = `Request received | ${method} ${url} | Time: ${date} | IP = ${ip} | UA = ${userAgent}`;
     console.log( message );
 
-    next();
+    next(); // control transfers to the next middleware
+
+    const dateOfResponse = (new Date()).toString();
+    const messageResponse = `Response sent | ${method} ${url} | Time: ${dateOfResponse} | IP = ${ip} | UA = ${userAgent}`;
+    console.log( messageResponse );
 };
 
 module.exports = logger;
