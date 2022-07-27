f88a5551a4698f69705277b70f11f424df5d74bc
Prashanth Puranik <puranik@digdeeper.in>
2022-07-27 18:54:42 +0530

    sending error response

--- demos/06-express/src/controllers/api/workshops.controller.js
+++ demos/06-express/src/controllers/api/workshops.controller.js
@@ -7,15 +7,29 @@ const {
 const getWorkshops = ( req, res ) => {
     const workshops = getAllWorkshops();
     // send(), redirect(), json(), sendFile(), render()
-    res.status( 200 ).json( workshops );
+    res.status( 200 ).json({
+        status: 'success',
+        data: workshops
+    });
 };
 
 const getWorkshopById = ( req, res ) => {
     const id = +req.params.id;
 
     const workshop = getWorkshopByIdSvc( id );
+
+    if( !workshop ) {
+        res.status( 404 ).json({
+            status: 'error',
+            message: `Workshop with id = ${id} does not exist`
+        });
+        return;
+    }
     
-    res.status( 200 ).json( workshop );
+    res.status( 200 ).json({
+        status: 'success',
+        data: workshop
+    });
 };
 
 const postWorkshop = ( req, res ) => {
