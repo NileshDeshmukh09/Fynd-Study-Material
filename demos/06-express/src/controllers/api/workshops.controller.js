33df98019cbcd42b6716b9fdd386c52dbffd5dab
Prashanth Puranik <puranik@digdeeper.in>
2022-07-27 18:47:37 +0530

    GET / api/workshops/:id

--- demos/06-express/src/controllers/api/workshops.controller.js
+++ demos/06-express/src/controllers/api/workshops.controller.js
@@ -1,9 +1,21 @@
-const { getAllWorkshops } = require( '../../services/workshops.service' );
+const {
+    getAllWorkshops,
+    // renaming while destructuring since there is a function with the same name in this file as well
+    getWorkshopById : getWorkshopByIdSvc
+} = require( '../../services/workshops.service' );
 
 const getWorkshops = ( req, res ) => {
     const workshops = getAllWorkshops();
     // send(), redirect(), json(), sendFile(), render()
     res.status( 200 ).json( workshops );
+};
+
+const getWorkshopById = ( req, res ) => {
+    const id = +req.params.id;
+
+    const workshop = getWorkshopByIdSvc( id );
+    
+    res.status( 200 ).json( workshop );
 };
 
 const postWorkshop = ( req, res ) => {
@@ -12,5 +24,6 @@ const postWorkshop = ( req, res ) => {
 
 module.exports = {
     getWorkshops,
-    postWorkshop
+    postWorkshop,
+    getWorkshopById
 };
