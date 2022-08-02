1bb2a9459d42d1a46848730382a606e41886223e
Prashanth Puranik <puranik@digdeeper.in>
2022-08-02 18:34:04 +0530

    iterating results using the cursor

--- demos/07-mongodb/07-cursor.js
+++ demos/07-mongodb/07-cursor.js
@@ -1,4 +1,8 @@
 use showsDB;
 
-const cursor = db.shows.find( {}, { name: true } );
+let cursor2 = db.shows.find( {}, { name: true } );
 
+// if there is a next document, let us pull it from the server and display it
+while( cursor2.hasNext() ) {
+    printjson( cursor2.next() );
+}
