4ecbc4b512196ffdbb781a238ee092f6efa2b146
Prashanth Puranik <puranik@digdeeper.in>
2022-08-10 20:33:07 +0530

    Note on HTML file upload structure

--- demos/06-express/src/routes/api/users.routes.js
+++ demos/06-express/src/routes/api/users.routes.js
@@ -9,6 +9,11 @@ const router = express.Router();
 
 router.post( '/register', register );
 router.post( '/login', login );
+// 'profilePic' is the 'name' of the field in the form used to upload the file
+// In frontend, for HTML
+// <form action="/api/auth/upload-profile-image" method="POST" enctype="multipart/form-data">
+//      <input type="file" name="profilePic" />
+// </form>
 router.post( '/upload-profile-image', upload.single( 'profilePic' ), uploadProfileImage );
 
 module.exports = router;
